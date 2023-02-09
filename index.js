ee9ff7d587fd3b5dc37acf6736307391a71e4c86
İhsan Cemil Çiçek <ihsancemil.cicek@trendyol.com>
2023-02-09 20:57:06 +0300

    cleanup the code

--- index.js
+++ index.js
@@ -7,50 +7,57 @@ console.log("starting")
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(new RegExp("start", "i"), async (welcomeMessage) => {
+async function handleInitialMessage(initialMessage) {
+    const opts = {reply_markup: {force_reply: true}};
+
+    let statusMessageRequest = await bot.sendMessage(welcomeMessage.from.id, 'Durumunuzu paylaşabilir misiniz?', opts);
+    bot.onReplyToMessage(welcomeMessage.chat.id, statusMessageRequest.message_id, handleStatusMessage)
+}
+
+async function handleStatusMessage(statusMessage) {
     const opts = {
         reply_markup: {
-            force_reply: true
+            keyboard: [[
+                {text: 'Konum paylaş', request_location: true}
+            ]]
         }
     };
 
-    let statusMessageRequest = await bot.sendMessage(welcomeMessage.from.id, 'Durumunuzu paylaşabilir misiniz?', opts);
-    bot.onReplyToMessage(welcomeMessage.chat.id, statusMessageRequest.message_id, async (statusMessage) => {
-        const opts = {
-            reply_markup: {
-                keyboard: [
-                    [
-                        {
-                            text: 'Konum paylaş',
-                            request_location: true
-                        }
-                    ]
+    let locationMessageRequest = await bot.sendMessage(statusMessage.from.id, 'Konum bilgilerinizi paylaşabilir misiniz?', opts);
+    bot.onReplyToMessage(
+        statusMessage.chat.id,
+        locationMessageRequest.message_id,
+        (locationMessage) => handleLocationMessage(statusMessage, locationMessage)
+    )
+}
+
+async function handleLocationMessage(statusMessage, locationMessage) {
+    const opts = {
+        reply_markup: {
+            one_time_keyboard: true,
+            keyboard: [
+                [
+                    {text: 'Telefon paylaş', request_contact: true}
                 ]
-            }
-        };
+            ]
+        }
+    };
 
-        let locationMessageRequest = await bot.sendMessage(statusMessage.from.id, 'Konum bilgilerinizi paylaşabilir misiniz?', opts);
-        bot.onReplyToMessage(statusMessage.chat.id, locationMessageRequest.message_id, async (locationMessage) => {
-            const opts = {
-                reply_markup: {
-                    one_time_keyboard: true,
-                    keyboard: [
-                        [
-                            {
-                                text: 'Telefon paylaş',
-                                request_contact: true,
-                            }
-                        ]
-                    ]
-                }
-            };
+    let contactMessageRequest = await bot.sendMessage(locationMessage.chat.id, "İletişim bilgilerinizi paylaşabilir misiniz?", opts)
+    bot.onReplyToMessage(
+        locationMessage.chat.id,
+        contactMessageRequest.message_id,
+        (contactMessage) => handleContactMessage(statusMessage, locationMessage, contactMessage)
+    )
+}
 
-            let contactMessageRequest = await bot.sendMessage(locationMessage.chat.id, "İletişim bilgilerinizi paylaşabilir misiniz?", opts)
-            bot.onReplyToMessage(locationMessage.chat.id, contactMessageRequest.message_id, async (contactMessage) => {
-                console.log(statusMessage.text, locationMessage.location, contactMessage.contact)
-                await bot.sendMessage(contactMessage.chat.id, "Bilgileriniz alınmıştır teşekkürler.")
-            })
-        })
-    })
-});
+async function handleContactMessage(statusMessage, locationMessage, contactMessage) {
+    console.log(statusMessage.text, locationMessage.location, contactMessage.contact)
+    await bot.sendMessage(
+        contactMessage.chat.id,
+        "Bilgileriniz alınmıştır teşekkürler."
+    )
+}
+
+bot.onText(new RegExp("start", "i"), handleInitialMessage);
 
